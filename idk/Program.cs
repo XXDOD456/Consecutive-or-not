c5828e804d0a5294ead49aaef95743372f43aa61
XXDOD456 <kylemcconnell94@gmail.com>
2020-05-22 08:17:03 -0500

    I forgot about this

---
+++ idk/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace idk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers separate by hypen : ");
+            var name = Console.ReadLine();
+            var numarray = name.Split('-');
+            int firstValue = Convert.ToInt32(numarray[0]);
+
+            bool cons = true;
+            for (var i = 0; i < numarray.Length; i++)
+            {
+                if (Convert.ToInt32(numarray[i]) - i != firstValue)
+                {
+                    cons = false;
+                    break;
+                }
+            }
+            if (cons)
+            {
+                Console.WriteLine("Consecutive");
+            }
+            else
+            {
+                Console.WriteLine("Not Consecutive");
+            }
+        }
+    }
+}
